229cf4654b560884ddbc468263956ab7da528d61
zhoulin-cirlce <zlinjiejie@sina.com>
2017-11-14 20:17:45 +0800

    模板测试

--- myapp/lib/demo.js
+++ myapp/lib/demo.js
@@ -17,42 +17,42 @@
 // }
 // MongoClient.connect(DB_CONN_STR, function(err, db) {
 //     console.log("连接成功！");
-    // insertData(db, function(result) {
-    //     console.log(result);
-    //     db.close();
-    // });
+// insertData(db, function(result) {
+//     console.log(result);
+//     db.close();
+// });
 // });
 
 //建立与数据库的连接
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/test")
 var db = mongoose.connection;
-db.on("error",console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
     console.log("已连接！")
 });
 
 //查看
-module.exports.findAll=function(req,res,next){
-    var data = [{"name":"周麟","url":"www.baidu.com"},{"name":"珂玥","url":"www.hao123.com"}];
+module.exports.findAll = function (req, res, next) {
+    var data = [{ "name": "周麟", "url": "www.baidu.com" }, { "name": "珂玥", "url": "www.hao123.com" }];
     db.collection('site').insert(data)
-    db.collection('site').update({"name":"周麟"},{$set:{"url":"www.zk.com"}},{multi:true})
-    res.render('index', { title: 'tit',author:'kk0'});
+    db.collection('site').update({ "name": "周麟" }, { $set: { "url": "www.zk.com" } }, { multi: true })
+    res.render('index', { title: 'tit', author: 'kk0' });
 }
 //添加
-module.exports.insert=function(req,res,next){
-     res.render('users', { title: '添加',author:'kk1'});
+module.exports.insert = function (req, res, next) {
+    res.render('users', { title: '添加', author: 'kk1' });
 }
 //根据id
-module.exports.getById=function(req,res,next){
-    res.render('users',{title:'查看',author:"123456"});
+module.exports.getById = function (req, res, next) {
+    res.render('users', { title: '查看', author: "123456" });
 
 }
 //删除
-module.exports.remove=function(req,res,next){
-    res.render('users',{title:'删除',author:'delete circle!'});
+module.exports.remove = function (req, res, next) {
+    res.render('users', { title: '删除', author: 'delete circle!' });
 }
 //修改
-module.exports.update=function(req,res,next){
-    res.render('users',{title:'修改',author:'circle to keyue'});
+module.exports.update = function (req, res, next) {
+    res.render('users', { title: '修改', author: 'circle to keyue' });
 }
---
+++ myapp/lib/module/mongodb.js
@@ -0,0 +1,9 @@
+// //建立与数据库的连接
+// var mongoose=require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/test")
+// var db = mongoose.connection;
+// db.on("error",console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//     console.log("已连接！")
+// });
+// exports.mongoose=mongoose;
